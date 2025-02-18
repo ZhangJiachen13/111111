6b2a9c32d5e2c4bb7a9ae08580f678497d62e3c3
ZhangJiachen13 <1518048969@qq.com>
2025-02-18 20:50:20 +0800

    project

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("This is my first java project");
+        System.out.println("This is my java project");
     }
 }
